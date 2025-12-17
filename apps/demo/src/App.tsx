340434ecf0d404d18e20f8050a6f68de539887d1
Karel Švancar <karell.svancar@gmail.com>
2025-12-17 20:54:32 +0100

    Init project

---
+++ apps/demo/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@react-component-kit/ui"
+
+function App() {
+    return (
+        <div style={{ padding: 40 }}>
+            <h1>Demo</h1>
+            <Button>Library Button</Button>
+        </div>
+    )
+}
+
+export default App
---
+++ library/ui/src/Button.ts
@@ -0,0 +1,9 @@
+import React from "react"
+
+export type ButtonProps = {
+    children?: React.ReactNode
+}
+
+export const Button = () => {
+    return "sss"
+}
